71be92808bfd63c9c5f03dff2814238565b112ca
fominykh.d <fominykh.d@sintels.ru>
2022-10-28 16:10:46 +0300

    routes to main screen and back

--- lib/app/routes/routes.dart
+++ lib/app/routes/routes.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_template/ui/screens/main_screen.dart';
+
+import '../../ui/screens/main_screen.dart';
+import '../../ui/screens/sign_in_screen.dart';
 
 class Routes {
   static const main = 'main';
+  static const signIn = 'signIn';
 
   static final Map<String, Widget> _ways = {
     main: const MainScreen(),
+    signIn: const SignInScreen(),
   };
 
   static final Map<String, WidgetBuilder> routes =
--- lib/ui/screens/main_screen.dart
+++ lib/ui/screens/main_screen.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+
+import '../../app/routes/routes.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+              onPressed: () => Navigator.of(context).popAndPushNamed(Routes.signIn),
+              icon: const Icon(Icons.exit_to_app_sharp))
+        ],
+      ),
+    );
   }
 }
