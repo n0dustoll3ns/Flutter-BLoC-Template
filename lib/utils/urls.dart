45e9ed9ccd34eb15cccece9f351bc3e1778c3b9f
n0dustoll3ns <fominykh.dg@gmail.com>
2023-01-22 21:48:47 +0300

    setup selection with local or remote database url

--- lib/utils/urls.dart
+++ lib/utils/urls.dart
@@ -1,15 +1,16 @@
 import 'package:pocketbase/pocketbase.dart';
 
-PocketBase pb = PocketBase(localUrl);
-
 const baseUrl = "https://pocketbase.dancheg97.ru";
 const localUrl = "http://127.0.0.1:8090";
+const currentUrl = localUrl;
 const filesUrl = "/api/files";
-const categoriesFilesUrl = "$baseUrl$filesUrl/categories";
-const productsFilesUrl = "$baseUrl$filesUrl/products";
-const mainBannerFilesUrl = "$baseUrl$filesUrl/main_banners";
-const secondaryBannerFilesUrl = "$baseUrl$filesUrl/secondary_banners";
-const tizersIconsUrl = "$baseUrl$filesUrl/tizers";
-const brandsImagesUrl = "$baseUrl$filesUrl/brands";
-const promoImagesUrl = "$baseUrl$filesUrl/promo";
-const aboutShopImagesUrl = "$baseUrl$filesUrl/about_shop";
+const categoriesFilesUrl = "$currentUrl$filesUrl/categories";
+const productsFilesUrl = "$currentUrl$filesUrl/products";
+const mainBannerFilesUrl = "$currentUrl$filesUrl/main_banners";
+const secondaryBannerFilesUrl = "$currentUrl$filesUrl/secondary_banners";
+const tizersIconsUrl = "$currentUrl$filesUrl/tizers";
+const brandsImagesUrl = "$currentUrl$filesUrl/brands";
+const promoImagesUrl = "$currentUrl$filesUrl/promo";
+const aboutShopImagesUrl = "$currentUrl$filesUrl/about_shop";
+
+PocketBase pb = PocketBase(currentUrl);
